2c156e9c257ef3002c69476c21118e7c79fa65c2
marianaBolivar24 <marianadiazbolivar1@gmail.com>
2024-10-07 21:25:25 -0500

    Funciones finales

--- app/src/main/java/com/example/movb02_grupo2_pl/PerfilActivity.kt
+++ app/src/main/java/com/example/movb02_grupo2_pl/PerfilActivity.kt
@@ -1,6 +1,9 @@
 package com.example.movb02_grupo2_pl
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class PerfilActivity: AppCompatActivity() {
@@ -8,5 +11,27 @@ class PerfilActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.perfil)
+
+        val gurdarButton = findViewById<Button>(R.id.button_guardar)
+        val cerrarSesion_button = findViewById<Button>(R.id.button_csesion)
+
+
+        gurdarButton.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java) // Usar la importación
+            startActivity(intent)
+            finish()
+
+        }
+
+        cerrarSesion_button.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java) // Usar la importación
+            startActivity(intent)
+            finish()
+
+        }
     }
+
+
 }

27dd54b96eafbc7136f7a38fb1aa003d72f7df75
marianaBolivar24 <marianadiazbolivar1@gmail.com>
2024-10-07 21:11:55 -0500

    Funcion Galeria y Video

--- app/src/main/java/com/example/movb02_grupo2_pl/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/movb02_grupo2_pl/ui/slideshow/SlideshowFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.movb02_grupo2_pl.R
 import com.example.movb02_grupo2_pl.databinding.FragmentSlideshowBinding
+
+
 
 class SlideshowFragment : Fragment() {
 
@@ -28,12 +33,19 @@ class SlideshowFragment : Fragment() {
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val webView = binding.video  // "video" es el ID en tu archivo XML
+        val video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/vIQtQjM-9xQ?si=rlIVkGfDAGEKMz1P\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+
+        // Configura tu WebView aquí si es necesario
+        webView.loadData(video, "text/html", "utf-8")
+        webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = WebChromeClient()
+
+
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
